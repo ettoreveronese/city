d831fceacd0193f2cd5fe4664a569a314f1c929d
Z0d1k <10151@dalcero.edu.it>
2026-05-29 17:55:19 +0200

    Pulito certi file

--- scratch/TestFactory.java
+++
@@ -1,9 +0,0 @@
-import com.cityproject.model.factory.BuildingFactory;
-import com.cityproject.model.type.BuildingType;
-
-public class TestFactory {
-    public static void main(String[] args) {
-        BuildingType type = BuildingFactory.getInstance().getBuildingType("CONDO");
-        System.out.println("CONDO unlockLevel: " + (type != null ? type.getUnlockLevel() : "null"));
-    }
-}
